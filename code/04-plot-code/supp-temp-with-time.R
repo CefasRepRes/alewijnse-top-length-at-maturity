db2ddd0bb705ecfc6b0390a5b8676fc6f8b7281d
Sarah Alewijnse <sarah.alewijnse@cefas.gov.uk>
2025-09-11 11:42:22 +0100

    Update axis name

--- code/04-plot-code/supp-temp-with-time.R
+++ code/04-plot-code/supp-temp-with-time.R
@@ -13,18 +13,18 @@ str(dat)
 dat[, yr := lubridate::year(catch_date)]
 
 # plot
-dd_time_plot <- ggplot(data = dat, aes(x = yr, y = dd_scaled, group = yr)) +
+dm_time_plot <- ggplot(data = dat, aes(x = yr, y = dm_scaled, group = yr)) +
   geom_boxplot(alpha = 0.5) +
-  geom_hline(aes(yintercept = mean(dd_scaled)),
+  geom_hline(aes(yintercept = mean(dm_scaled)),
              linetype = "dashed") +
   xlab("Catch year") +
-  ylab("Scaled degree months") +
+  ylab("Degree month anomalies") +
   scale_x_continuous(breaks = seq(2010, 2023, 2)) +
   scale_y_continuous(breaks = seq(-200, 400, 50)) +
   theme_bw()
-dd_time_plot
+dm_time_plot
 
 png(here::here("outputs", "plots", "dm_with_year.png"),
     width = 8, height = 6, units = "in", res = 250)
-dd_time_plot
+dm_time_plot
 dev.off()
