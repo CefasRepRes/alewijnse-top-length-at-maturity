b3f5d3dfc55b062dc8f7fcf6e0e7f096a65b804f
Sarah Alewijnse <sarah.alewijnse@cefas.gov.uk>
2025-09-22 16:21:31 +0100

    Compare gonadal stage to AAM

--- code/01-data-wrangling/03-data-summary-table.R
+++ code/01-data-wrangling/03-data-summary-table.R
@@ -18,3 +18,17 @@ dat_summary_yr
 dat_summary <- dat[, .N,
                    by = c("dataset")]
 dat_summary
+
+# AAM comparison -----
+
+# females
+dat_f_below <- dat[Sex == "Female" & Age < 14]
+dat_f_below_summary <- dat_f_below[, .N, by = "Maturity"]
+dat_f_below_summary[, prop := N / sum(N)]
+dat_f_below_summary
+
+# males
+dat_m_below <- dat[Sex == "Male" & Age < 9]
+dat_m_below_summary <- dat_m_below[, .N, by = "Maturity"]
+dat_m_below_summary[, prop := N / sum(N)]
+dat_m_below_summary
